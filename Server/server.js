5d974f018165b96787235031f8053b3862983a41
qpohj <28758232+qpohj@users.noreply.github.com>
2024-07-10 13:29:44 +0100

    updated

--- Server/server.js
+++ Server/server.js
@@ -1,36 +1,38 @@
 const express = require("express");
 const cors = require("cors")
 const dotenv = require('dotenv')
-
+const fs = require("fs");
+const { schema, schemaRemoveFav, schemaAddFav } = require("./src/validateUser");
+const Joi = require("joi");
 
 dotenv.config();
 const app = express();
 const port = process.env.SERVER_PORT || 3002;
 
 app.use(cors({
-    origin: 'http://localhost:5173'
+  origin: 'http://localhost:5173'
 }))
 app.use(express.json());
 
 
 
 app.get('/api/search', async (req, res) => {
-    const query = req.query.q;
-    try {
-      const response = await axios.get('https://www.googleapis.com/customsearch/v1', {
-        params: {
-          key: AIzaSyATn8-VgSOORxBmA2o2wMvqscWSxaIVb6I,
-          cx: c244fc5dc3f57416b,
-          q: query,
-          searchType: 'image',
-          num: 10,
-        },
-      });
-      res.json(response.data.items);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'An error occurred while searching' });
-    }
-  });
+  const query = req.query.q;
+  try {
+    const response = await axios.get('https://www.googleapis.com/customsearch/v1', {
+      params: {
+        key: AIzaSyATn8-VgSOORxBmA2o2wMvqscWSxaIVb6I,
+        cx: c244fc5dc3f57416b,
+        q: query,
+        searchType: 'image',
+        num: 10,
+      },
+    });
+    res.json(response.data.items);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while searching' });
+  }
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
---
+++ Server/src/validateUser.js
@@ -0,0 +1,15 @@
+const Joi = require("joi");
+
+const schema = Joi.object({
+    
+});
+
+const schemaRemoveFav = Joi.object({
+    
+});
+
+const schemaAddFav = Joi.object({
+    
+});
+
+module.exports = { schema, schemaRemoveFav, schemaAddFav };
